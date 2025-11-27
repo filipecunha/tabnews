4408727f687954ef407610dd510e1c26b6aaac87
Filipe Cunha <filipe.cunha@gmail.com>
2025-11-27 20:07:31 -0300

    make `database.js handle with query erros`

--- infra/database.js
+++ infra/database.js
@@ -10,11 +10,15 @@ const query = async (queryObject) => {
 	});
 
 	await client.connect();
-	const result = await client.query(queryObject);
 
-	await client.end();
-
-	return result;
+	try {
+		const result = await client.query(queryObject);
+		return result;
+	} catch (error) {
+		console.error(error);
+	} finally {
+		await client.end();
+	}
 };
 
 export default {
